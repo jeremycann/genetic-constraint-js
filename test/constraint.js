1a372b0ef7e61991a461ebc2536fbe34e7044dae
Jeremy Cannon <jeremycann@gmail.com>
2021-11-04 13:33:59 -0400

    WIP: Add conflicting constraint test

--- test/constraint.js
+++ test/constraint.js
@@ -47,60 +47,61 @@ beforeEach('Setting Up Genetic', function () {
   genetic = Genetic.create();
   genetic.optimize = Genetic.Optimize.Maximize;
   genetic.select1 = Genetic.Select1.RandomLinearRank;
-  genetic.select2 = Genetic.Select2.Tournament2
+  genetic.select2 = Genetic.Select2.Tournament2;
+
+  genetic.seed = function () {
+    return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  };
+  genetic.mutate = function (entity) {
+    const i = Math.floor(Math.random() * entity.length);
+    const flipValue = (1 - entity[i]);
+    entity.splice(i, 1, flipValue);
+
+    return entity;
+  };
+  genetic.crossover = function (mother, father) {
+    // two-point crossover
+    const len = mother.length;
+    let ca = Math.floor(Math.random() * len);
+    let cb = Math.floor(Math.random() * len);
+    if (ca > cb) {
+      const tmp = cb;
+      cb = ca;
+      ca = tmp;
+    }
+
+    const fatherBegin = father.slice(0, ca);
+    const fatherMid = father.slice(ca, cb);
+    const fatherEnd = father.slice(cb);
+
+    const motherBegin = mother.slice(0, ca);
+    const motherMid = mother.slice(ca, cb);
+    const motherEnd = mother.slice(cb);
+
+    const son = [
+      ...fatherBegin,
+      ...motherMid,
+      ...fatherEnd
+    ];
+
+    const daughter = [
+      ...motherBegin,
+      ...fatherMid,
+      ...motherEnd
+    ];
+
+    return [son, daughter];
+  };
+  genetic.fitness = function (entity) {
+    return entity.reduce((acc, single) => {
+      acc += single;
+      return acc;
+    }, 0)
+  };
 });
 
 describe("Constraints", function () {
   it("Must Have One : Position 1", function () {
-    genetic.seed = function () {
-      return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    };
-    genetic.mutate = function (entity) {
-      const i = Math.floor(Math.random() * entity.length);
-      const flipValue = (1 - entity[i]);
-      entity.splice(i, 1, flipValue);
-
-      return entity;
-    };
-    genetic.crossover = function (mother, father) {
-      // two-point crossover
-      const len = mother.length;
-      let ca = Math.floor(Math.random() * len);
-      let cb = Math.floor(Math.random() * len);
-      if (ca > cb) {
-        const tmp = cb;
-        cb = ca;
-        ca = tmp;
-      }
-
-      const fatherBegin = father.slice(0, ca);
-      const fatherMid = father.slice(ca, cb);
-      const fatherEnd = father.slice(cb);
-
-      const motherBegin = mother.slice(0, ca);
-      const motherMid = mother.slice(ca, cb);
-      const motherEnd = mother.slice(cb);
-
-      const son = [
-        ...fatherBegin,
-        ...motherMid,
-        ...fatherEnd
-      ];
-
-      const daughter = [
-        ...motherBegin,
-        ...fatherMid,
-        ...motherEnd
-      ];
-
-      return [son, daughter];
-    };
-    genetic.fitness = function (entity) {
-      return entity.reduce((acc, single) => {
-        acc += single;
-        return acc;
-      }, 0)
-    };
     genetic.generation = function (pop, generation, stats, isFinished) {
       if (generation > 1) {
         for (let i = 0; i < pop.length; i++) {
@@ -127,55 +128,6 @@ describe("Constraints", function () {
   });
 
   it("Must Have One : Multiple : Position 0, 1, 3", function () {
-    genetic.seed = function () {
-      return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    };
-    genetic.mutate = function (entity) {
-      const i = Math.floor(Math.random() * entity.length);
-      const flipValue = (1 - entity[i]);
-      entity.splice(i, 1, flipValue);
-
-      return entity;
-    };
-    genetic.crossover = function (mother, father) {
-      // two-point crossover
-      const len = mother.length;
-      let ca = Math.floor(Math.random() * len);
-      let cb = Math.floor(Math.random() * len);
-      if (ca > cb) {
-        const tmp = cb;
-        cb = ca;
-        ca = tmp;
-      }
-
-      const fatherBegin = father.slice(0, ca);
-      const fatherMid = father.slice(ca, cb);
-      const fatherEnd = father.slice(cb);
-
-      const motherBegin = mother.slice(0, ca);
-      const motherMid = mother.slice(ca, cb);
-      const motherEnd = mother.slice(cb);
-
-      const son = [
-        ...fatherBegin,
-        ...motherMid,
-        ...fatherEnd
-      ];
-
-      const daughter = [
-        ...motherBegin,
-        ...fatherMid,
-        ...motherEnd
-      ];
-
-      return [son, daughter];
-    };
-    genetic.fitness = function (entity) {
-      return entity.reduce((acc, single) => {
-        acc += single;
-        return acc;
-      }, 0)
-    };
     genetic.generation = function (pop, generation, stats, isFinished) {
       if (generation > 1) {
         for (let i = 0; i < pop.length; i++) {
@@ -207,55 +159,6 @@ describe("Constraints", function () {
   });
 
   it("Must Have Multiple Constraints", function () {
-    genetic.seed = function () {
-      return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    };
-    genetic.mutate = function (entity) {
-      const i = Math.floor(Math.random() * entity.length);
-      const flipValue = (1 - entity[i]);
-      entity.splice(i, 1, flipValue);
-
-      return entity;
-    };
-    genetic.crossover = function (mother, father) {
-      // two-point crossover
-      const len = mother.length;
-      let ca = Math.floor(Math.random() * len);
-      let cb = Math.floor(Math.random() * len);
-      if (ca > cb) {
-        const tmp = cb;
-        cb = ca;
-        ca = tmp;
-      }
-
-      const fatherBegin = father.slice(0, ca);
-      const fatherMid = father.slice(ca, cb);
-      const fatherEnd = father.slice(cb);
-
-      const motherBegin = mother.slice(0, ca);
-      const motherMid = mother.slice(ca, cb);
-      const motherEnd = mother.slice(cb);
-
-      const son = [
-        ...fatherBegin,
-        ...motherMid,
-        ...fatherEnd
-      ];
-
-      const daughter = [
-        ...motherBegin,
-        ...fatherMid,
-        ...motherEnd
-      ];
-
-      return [son, daughter];
-    };
-    genetic.fitness = function (entity) {
-      return entity.reduce((acc, single) => {
-        acc += single;
-        return acc;
-      }, 0)
-    };
     genetic.generation = function (pop, generation, stats, isFinished) {
       if (generation > 1) {
         for (let i = 0; i < pop.length; i++) {
@@ -286,55 +189,6 @@ describe("Constraints", function () {
   });
 
   it("Must-have and Must-exclude Constraints", function () {
-    genetic.seed = function () {
-      return [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    };
-    genetic.mutate = function (entity) {
-      const i = Math.floor(Math.random() * entity.length);
-      const flipValue = (1 - entity[i]);
-      entity.splice(i, 1, flipValue);
-
-      return entity;
-    };
-    genetic.crossover = function (mother, father) {
-      // two-point crossover
-      const len = mother.length;
-      let ca = Math.floor(Math.random() * len);
-      let cb = Math.floor(Math.random() * len);
-      if (ca > cb) {
-        const tmp = cb;
-        cb = ca;
-        ca = tmp;
-      }
-
-      const fatherBegin = father.slice(0, ca);
-      const fatherMid = father.slice(ca, cb);
-      const fatherEnd = father.slice(cb);
-
-      const motherBegin = mother.slice(0, ca);
-      const motherMid = mother.slice(ca, cb);
-      const motherEnd = mother.slice(cb);
-
-      const son = [
-        ...fatherBegin,
-        ...motherMid,
-        ...fatherEnd
-      ];
-
-      const daughter = [
-        ...motherBegin,
-        ...fatherMid,
-        ...motherEnd
-      ];
-
-      return [son, daughter];
-    };
-    genetic.fitness = function (entity) {
-      return entity.reduce((acc, single) => {
-        acc += single;
-        return acc;
-      }, 0)
-    };
     genetic.generation = function (pop, generation, stats, isFinished) {
       if (generation > 1) {
         for (let i = 0; i < pop.length; i++) {
@@ -361,6 +215,34 @@ describe("Constraints", function () {
     const constraints = {
       mustHave: mustHaveConstraint([1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
       mustExclude: mustExludeConstraint([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1])
+    }
+
+    genetic.evolve(config, {}, constraints);
+  });
+
+  it("Conflicting Constraints - Last Must Exclude", function () {
+    genetic.generation = function (pop, generation, stats, isFinished) {
+      if (generation > 1) {
+        for (let i = 0; i < pop.length; i++) {
+          const singleGeneration = pop[i].entity;
+
+          for (let i = 0; i < singleGeneration.length; i++) {
+            assert.equal(singleGeneration[i], 0);
+          }
+        }
+      }
+    }
+
+    const config = {
+      iterations: 100,
+      size: 100,
+      crossover: 0.7,
+      mutation: 0.7,
+    };
+
+    const constraints = {
+      mustHave: mustHaveConstraint([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]),
+      mustExclude: mustExludeConstraint([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
     }
 
     genetic.evolve(config, {}, constraints);
